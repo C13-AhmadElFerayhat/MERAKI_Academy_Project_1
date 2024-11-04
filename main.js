bf443e5b17a63c7973ef80b61d4aecfaf2d9900c
AhmadElFerayhat <a.elferayhat@gmail.com>
2024-11-04 03:03:54 +0300

    programming the game is finshed

--- main.js
+++ main.js
@@ -9,42 +9,65 @@ const alphabet = [
     "z",
   ];
 
-  const Q = [
+  const  Q = [
     {
         id: 1,
         c: "man",
-        name: ["c","a","t"],
-        name1: "cat"
+        name: ["c","a","a","c","t","m"],
+        name1: "cat",
+        r: 4
     },
     {
         id: 2,
         c: "sss",
-        name: ["c","a","t"]
-    },{
-        id: 2,
-        c: "ddd",
-        name: ["c","a","t"]
+        name: ["c","a","t"],
+        r: 3
     },{
         id: 3,
+        c: "ddd",
+        name: ["c","a","t"],
+        r: 3
+    },{
+        id: 4,
         c: "fff",
-        name: ["c","a","t"]
+        name: ["c","a","t"],
+        r: 3
     },
   ]
   const images = [
-    { id: 1, src: "" /*add more key/value if need*/ },
-    { id: 2, src: "" /*add more key/value if need*/ },
+    { id: 0, src: "New folder/Untitled-2.png" },
+    { id: 1, src: "New folder/Untitled-3.png" },
+    { id: 2, src: "New folder/Untitled-4.png" },
+    { id: 3, src: "New folder/Untitled-5.png" },
+    { id: 4, src: "New folder/Untitled-6.png" },
+    { id: 5, src: "New folder/Untitled-8.png" },
+    { id: 5, src: "New folder/Untitled-9.png" },
   ];
+
 const body = document.querySelector("body")
 const game = document.querySelector("#game")
 const clue = document.querySelector("#clue")
 const start1 = document.querySelector("#start")
 const Alpha = document.querySelector("#Alpha")
 const alpha = document.querySelector("#alpha")
+const imgMain = document.getElementById("imgMain")
+let r1 = 0
+let n1 = 0
+let  Q1 =  Q
   const start = () =>{
-    let q = Math.round(Math.random() * (Q.length - 1))
-clue.innerHTML = Q[q].c
+    clue.innerHTML = ""
+        alpha.innerHTML = ""
+        imgMain.innerHTML = ""
+        Alpha.innerHTML = ""
+    const firstIm = document.createElement("img")
+    firstIm.src = images[n1].src
+    imgMain.append(firstIm)
+    let q = Math.round(Math.random() * ( Q1.length - 1))
+    console.log(Q1);
+    
+clue.innerHTML = Q1[q].c
 let r;
-Q[q].name.forEach((element,i) => {
+  Q1[q].name.forEach((element,i) => {
     let s = document.createElement("dev")
     r = document.createElement("img")
     r.src = "New folder/mark.png"
@@ -59,19 +82,56 @@ w = document.createElement("p")
     w.innerText = element
     w.id = element + " uni"
     w.addEventListener("click", (e)=>{
-        Q[q].name.forEach ((element1,i) => {
-            if (e.target.innerText === element1) {
-           
-           let g = document.getElementById(i)
-           g.innerHTML = element1
-           let b = "#" + element1
-           i1.src = "New folder/T.png"
-           console.log(i1);
-            Alpha.replaceChild(i1, Alpha.children[i2])
-        }else{}
+      let t;
+         Q1[q].name.forEach ((element1,i) => {
+          
+          if (e.target.innerText === element1) {
+          let g = document.getElementById(i)
+          g.innerHTML = element1
+          i1.src = "New folder/T.png"
+        Alpha.replaceChild(i1, Alpha.children[i2])
+        t = 1
+        r1 = r1 + 1
+        }
     })
+    if(t === undefined)
+      {
+        i1.src = "New folder/F.png"
+        Alpha.children[i2]
+      Alpha.replaceChild(i1, Alpha.children[i2])
+      n1 = n1 + 1
+      }
+      if (n1 > -1 && n1 < 7) {  
+        firstIm.src = images[n1].src
+        imgMain.append(firstIm)
+      }else{
+        clue.innerHTML = ""
+        alpha.innerHTML = ""
+        imgMain.innerHTML = ""
+        Alpha.innerHTML = ""
+        const h1End = document.getElementById("h1End")
+        h1End.innerText = "u loose"
+      }
+      if (r1 ===  Q1[q].name.length) {
+        r1 = 0
+        clue.innerHTML = ""
+        alpha.innerHTML = ""
+        imgMain.innerHTML = ""
+        Alpha.innerHTML = ""
+        const h1End = document.getElementById("h1End")
+        h1End.innerText = "u completd this level"
+        Q1 = Q1.filter((e,i) => {
+          console.log(Q1[q].id, e.id);
+          
+          return Q1[q].id !== e.id
+        });
+        console.log(Q1);
+        
+      }
     })
 })
+
+
   }
 
   const Welcome = () =>{
@@ -79,4 +139,6 @@ w = document.createElement("p")
   } 
 
   start1.addEventListener("click", start)
+  
+  
   
